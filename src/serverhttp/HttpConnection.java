6d45f9b1646cf1802d8e01908d62bfbfb8446c78
Francisco <Corgi@CORGI-PORTATIL>
2018-12-05 12:04:26 +0100

    Subida con las excepciones

--- src/serverhttp/HttpConnection.java
+++ src/serverhttp/HttpConnection.java
@@ -18,20 +18,34 @@ public class HttpConnection implements Runnable{
     public void run() {
        DataOutputStream dos = null;
         try {
-            /*System.out.println("Nueva conexión http con: "+socket.getInetAddress().toString());
+           System.out.println("Server connection: "+socket.getInetAddress().toString());
             dos = new DataOutputStream(socket.getOutputStream());
-            dos.write("200 OK\r\n".getBytes());
-            dos.flush();*/
+            //dos.write("200 OK\r\n".getBytes());
+            dos.flush();
             
-             /*dos.write(("ECO"+line+"\r\n).getBytes());
-            dos.flush();*/
-            
+            //////////////////////
             BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String reqline = buffer.readLine();
+            System.out.println("SERVER ["+socket.getInetAddress().toString()+"] received>"+reqline);
+            String response="HTTP/1.1 200 OK\\r\\nContent-type:text/html\\r\\nConten-length:39\\r\\n\\r\\n";
+            String entity="<html><body><h1>HOLA</h1></body></html>";
+            try {
+           String path=  analizeRequest(reqline);
+            }catch (HttpExcepcion400 e400) {
+            	
+            }catch (HttpExcepcion404 e404) {
+            	
+            }catch (HttpExcepcion405 e405) {
+            	
+            }catch (HttpExcepcion505 e505) {
+            	
+            }
             
+            dos.write(("ECO "+reqline).getBytes());
+            dos.flush();
             String [] items=reqline.split(" ");
             if(items.length==3){ // Método, Recurso y Versión
-                if(items[2].equals("HTTP/0.9")||items[2].equals("HTTP/1.0")||items[2].equals("HTTP/1.1")||items[2].equals("HTTP/2.0")){ //Versiones posibles correctas
+                if(items[2].equals("HTTP/0.9")||items[2].equals("HTTP/1.0")||items[2].equals("HTTP/1.1")){ //Versiones posibles correctas //la cabecera http/2.0 no esta
                    
                     if(items[2].compareTo("HTTP/1.1")==0){
                         //La versión de HTTP es 1.1
@@ -46,13 +60,13 @@ public class HttpConnection implements Runnable{
                                 //BUSCAR EL RECURSO. SI EXISTE, DEVOLVERLO. SI NO EXISTE 404 Not Found
                                 
                             }else{
-                                //405 Method Not Allowed
+                                //505 HTTP Version Not Supported
                             }
                         }else{
-                            //400 Bad Request (formato método)
+                            //405 Method Not Allowed  
                         }
                     }else{
-                        //505 HTTP Version Not Supported
+                    	//400 Bad Request (formato método) 
                     }
                 }else{
                     //400 Bad Request (formato versión)
@@ -72,5 +86,27 @@ public class HttpConnection implements Runnable{
             }
         }
     }
+    protected String analizeRequest(String reqline) throws  HttpExcepcion400, HttpExcepcion404, HttpExcepcion405,HttpExcepcion505 {
+    	if (!reqline.startsWith("GET")) {throw new HttpExcepcion405();}
+    	return "/";
+		
+		
+    }
+    private String readEntity(String path) throws HttpExcepcion404{
+        //Leer del fichero
+        
+        return "<html><body><h1>405</h1></body></html>";
+        
+    }
     
+    public class HttpExcepcion400 extends IOException{
+    	
+    }
+public class HttpExcepcion404 extends IOException{
+    }
+public class HttpExcepcion405 extends IOException{
+	
+}
+public class HttpExcepcion505 extends IOException{
+}
 }
